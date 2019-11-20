e37ac75f71e1b3c7238658b1cd412a0e9c8d71d3
Uriel <urimeba511@gmail.com>
2019-11-20 08:16:46 -0600

    *added* Modelos

--- gqm/Apps/gqm/models.py
+++ gqm/Apps/gqm/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class Contexto(models.Model):
+    descripcion = models.CharField(max_length=1000)
+
+class Meta(models.Model):
+    descripcionGeneral = models.CharField(max_length=1000)
+    descripcionEspecifica = models.CharField(max_length=1000)
+    entidad = models.CharField(max_length=500)
+    proposito = models.CharField(max_length=500)
+    propiedades = models.CharField(max_length=500)
+    interesados = models.CharField(max_length=500)
+    ambiente = models.CharField(max_length=500)
+
+class MetaPregunta(models.Model):
+    meta = models.ForeignKey('Meta', on_delete=models.CASCADE)
+    pregunta = models.ForeignKey('Pregunta', on_delete=models.CASCADE)
+
+class Pregunta(models.Model):
+    pregunta = models.CharField(max_length=500)
